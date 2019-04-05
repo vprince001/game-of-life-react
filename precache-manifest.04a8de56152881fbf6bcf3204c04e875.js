4b9137bf0ed6f1364edc2d77b9ec30048a436212
Prince Verma <16836599+vprince001@users.noreply.github.com>
2019-04-05 15:46:42 +0530

    Updates

---
+++ precache-manifest.04a8de56152881fbf6bcf3204c04e875.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "830877e86d1c4e6d5be6",
+    "url": "/vprince001/game-of-life-react/static/js/runtime~main.6760fc89.js"
+  },
+  {
+    "revision": "68fa296b648b1a5100b9",
+    "url": "/vprince001/game-of-life-react/static/js/main.e326dd26.chunk.js"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/vprince001/game-of-life-react/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "68fa296b648b1a5100b9",
+    "url": "/vprince001/game-of-life-react/static/css/main.90207f0b.chunk.css"
+  },
+  {
+    "revision": "c9739138e16140900c5b05e7fb2b3bb2",
+    "url": "/vprince001/game-of-life-react/index.html"
+  }
+];
